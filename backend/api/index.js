c5f65bc62206cd16933dc6eebd5e86b12abfb38f
shailjayadav30 <shailjayadav7275@gmail.com>
2024-09-08 22:23:33 +0530

    modified

---
+++ backend/api/index.js
@@ -0,0 +1,5 @@
+import app from '../index.js';
+
+export default async (req, res) => {
+  app(req, res);  // Pass the request and response to Express app
+};
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@
 import express from "express";
 import { PORT, MongoUrl } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookmodels.js";
 import Booksrouter from "./route/Booksrouter.js"
 import cors from "cors"
+
+
 const app = express();
 // app.use(cors({
 //   origin:"http://localhost:5555",
@@ -23,12 +24,12 @@ app.get("/", (request, response) => {
 app.use("/books",Booksrouter)
 mongoose
   .connect(MongoUrl)
-  .then(() => {
-    console.log("App connected to database");
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT} `);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then(() => 
+    console.log("App connected to database"))
+  //   app.listen(PORT, () => {
+  //     console.log(`App is listening to port: ${PORT} `);
+  //   });
+  .catch((error) => 
+    console.error("MongoDB connection failed:", error))
+
+  export default app
